674b3ace1daeb6cfb064fba43f3498f5d9785e33
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2022-01-21 12:25:16 -0500

    runtime cache collection images

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
 
 clientsClaim();
 
@@ -48,16 +48,25 @@ registerRoute(
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
+// registerRoute(
+// 	// Add in any other file extensions or routing criteria as needed.
+// 	({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+// 	new StaleWhileRevalidate({
+// 		cacheName: 'images',
+// 		plugins: [
+// 			// Ensure that once this runtime cache reaches a maximum size the
+// 			// least-recently used images are removed.
+// 			new ExpirationPlugin({ maxEntries: 50 }),
+// 		],
+// 	})
+// );
+
+
 registerRoute(
-	// Add in any other file extensions or routing criteria as needed.
-	({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-	new StaleWhileRevalidate({
-		cacheName: 'images',
-		plugins: [
-			// Ensure that once this runtime cache reaches a maximum size the
-			// least-recently used images are removed.
-			new ExpirationPlugin({ maxEntries: 50 }),
-		],
+	// TODO: this should not be hardcoded but per-environment config, but is there even a staging for images?
+	({ url }) => url.hostname.toLowerCase() === 'images.metmuseum.org', // Customize this strategy as needed, e.g., by changing to CacheFirst.
+	new CacheFirst({
+		cacheName: 'collectionImages',
 	})
 );
 
