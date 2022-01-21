ee1bf8027080ebf301590282c0bda1cb64621ea9
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2022-01-21 18:30:49 -0500

    move callback

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { fetchObjects } from "./helpers/api";
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -11,12 +12,21 @@ ReactDOM.render(
 	document.getElementById('app')
 );
 
+const ensureSavedObjectsCache = () => {
+	const savedObjects = JSON.parse(localStorage.getItem('savedObjects')) || {};
+	console.log("running ensureSavedObjectsCache for:", savedObjects)
+	Object.keys(savedObjects).forEach(id => fetchObjects(id))
+}
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register().then(ensureSavedObjectsCache);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
