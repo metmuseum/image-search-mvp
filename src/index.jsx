7280a66c3223f562a181a4940f94a0cb6474e059
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2022-01-24 23:07:49 -0500

    i cant believe it's come to this

--- src/index.jsx
+++ src/index.jsx
@@ -15,9 +15,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-	onSuccess: warmUpTesseract
-});
+serviceWorkerRegistration.register();
+
+setTimeout(warmUpTesseract, 5000);
 
 
 // If you want to start measuring performance in your app, pass a function
