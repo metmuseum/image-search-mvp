7544d4d12f8e4e510475e3388be4e1a1219d2647
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2022-01-26 19:41:36 -0500

    add ability to detect when ready for offline mode

--- src/index.jsx
+++ src/index.jsx
@@ -14,16 +14,23 @@ ReactDOM.render(
 );
 
 const warmup = async () => {
-	warmUpTesseract();
-
-	setTimeout(()=> {
-		// haha race condition waiting for react to save these 😐
-		if (localStorage.getItem('savedObjects') !== null) {
-			let objectIds = Object.keys(JSON.parse(localStorage.getItem('savedObjects')));
-			warmUpObjectJSON(objectIds);
-		}
-	}, 3000)
+	console.log("getting ready for offline") // todo: make this a UI message, too
+	Promise.all(
+		[
+			warmUpTesseract(),
+			new Promise(resolve => setTimeout(()=> {
+				// haha race condition waiting for react to save these 😐
+				if (localStorage.getItem('savedObjects') !== null) {
+					let objectIds = Object.keys(JSON.parse(localStorage.getItem('savedObjects')));
+					resolve(warmUpObjectJSON(objectIds));
+				}
+			}, 3000)) // todo: fix by refactor to trigger this on a componentDidMount or something
+		]
+	).then(() => console.log("ready for offline!") // todo: make this a UI message, too
+	).catch(err=>console.error("there was an error warming up the cache for offline:", err))
 }
+
+warmup();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
